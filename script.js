3ff566ff09dee796638eca97f805f2baf7a62fd9
mrkmartin <martinmarkpaulo12@gmail.com>
2025-04-07 13:58:22 +0800

    Added some styling for responsiveness and added custom checkbox

--- script.js
+++ script.js
@@ -18,19 +18,55 @@ function createTask(text) {
   const left = document.createElement("div");
   left.classList.add("task-left");
 
+  const checkboxContainer = document.createElement("label");
+
   const checkbox = document.createElement("input");
+  const img = document.createElement("img");
+  img.src = "images/icon-check.svg";
+  img.classList.add("icon-check");
+
   checkbox.type = "checkbox";
+  checkbox.classList.add("custom-checkbox");
   checkbox.addEventListener("change", () => {
     li.classList.toggle("completed");
+
+    if (li.classList.contains("completed")) {
+      img.style.display = "block";
+      checkbox.style.background = `linear-gradient(
+  to bottom right,
+  hsl(192, 100%, 67%),
+  hsl(280, 87%, 65%)
+)`;
+    } else {
+      img.style.display = "none";
+      checkbox.style.background = "transparent";
+    }
     updateItemsLeft();
     applyFilter();
   });
 
+  img.addEventListener("click", () => {
+    li.classList.toggle("completed");
+    if (li.classList.contains("completed")) {
+      img.style.display = "block";
+      checkbox.style.background = `linear-gradient(
+  to bottom right,
+  hsl(192, 100%, 67%),
+  hsl(280, 87%, 65%)
+)`;
+    } else {
+      img.style.display = "none";
+      checkbox.style.background = "transparent";
+    }
+  });
+
   const span = document.createElement("span");
   span.textContent = text;
-
-  left.appendChild(checkbox);
+  checkboxContainer.appendChild(img);
+  checkboxContainer.appendChild(checkbox);
+  left.appendChild(checkboxContainer);
   left.appendChild(span);
+  left.appendChild(img);
 
   const deleteBtn = document.createElement("button");
   deleteBtn.classList.add("delete");
@@ -107,6 +143,8 @@ function applyFilter() {
 
 themeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark");
+
   const icon = document.querySelector("#theme-icon");
   const listItems = document.querySelectorAll("li");
 
